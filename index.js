2e54b2dff3b33575d77d1a926e12a6bfa6c7e24d
KeshonBowman <KeshonBowman@gmail.com>
2026-04-06 20:02:38 -0500

    LAST COMMIT ON MAIN

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Getting express module HTTP server running
 
 const express = require("express");
-
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Template engine
+app.set("view engine", app)
 
 app.get("/", (req, res) => {
     res.send("This is the Pit Stop Ministries API");
