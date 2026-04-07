9505f19dd3aa57a25b2edfd7dddd6280e503bcf4
KeshonBowman <KeshonBowman@gmail.com>
2026-04-06 21:15:18 -0500

    switching branches

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // Getting express module HTTP server running
 
+// Loading environment variables First - before anything reads process.env
+require("dotenv").config();
+
 const express = require("express");
+const path = require("path");
+
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Template engine
 app.set("view engine", app)
+
+// Parse JSON bodies ( Needed for POST requests)
+app.use(express.json());
+
 
 app.get("/", (req, res) => {
     res.send("This is the Pit Stop Ministries API");
